3f24a900c2193f557a9a282d3cf2fa510489b10b
Kévin Barré <kevinbarre90@gmail.com>
2023-12-04 21:38:29 +0100

    Day 04 - Part 2

--- Day04/main.py
+++ Day04/main.py
@@ -4,11 +4,14 @@ def part1(lines):
 
 
 def part2(lines):
-    return 0
+    cards = parse_cards(lines)
+    amount_of_each_card = get_amount_of_each_card(cards)
+    return sum(amount_of_each_card.values())
 
 
 class Card:
-    def __init__(self, winning_numbers, numbers):
+    def __init__(self, id, winning_numbers, numbers):
+        self.id = id
         self.winning_numbers = winning_numbers
         self.numbers = numbers
 
@@ -19,11 +22,12 @@ class Card:
 def parse_cards(lines):
     cards = []
     for line in lines:
-        _, all_numbers = line.split(':')
+        raw_identifier, all_numbers = line.split(':')
+        _, identifier = raw_identifier.split()
         raw_winning_numbers, raw_numbers = all_numbers.split('|')
         winning_numbers = {int(winning_number) for winning_number in raw_winning_numbers.split()}
         numbers = {int(number) for number in raw_numbers.split()}
-        cards.append(Card(winning_numbers, numbers))
+        cards.append(Card(int(identifier), winning_numbers, numbers))
     return cards
 
 
@@ -33,6 +37,18 @@ def get_points(card):
         return 2 ** (number_of_wins - 1)
     else:
         return 0
+
+
+def get_amount_of_each_card(cards):
+    cards_counter = {card.id: 1 for card in cards}
+    for card in cards:
+        matching_numbers = card.get_number_of_win()
+        # For each wining number, we get more of the below cards
+        # More specifically, we get one for each instance of the current card
+        current_card_counter = cards_counter[card.id]
+        for i in range(1, matching_numbers + 1):
+            cards_counter[card.id + i] += current_card_counter
+    return cards_counter
 
 
 if __name__ == '__main__':
--- Day04/test_main.py
+++ Day04/test_main.py
@@ -1,4 +1,4 @@
-from main import part1, part2, parse_cards, Card, get_points
+from main import part1, part2, parse_cards, Card, get_points, get_amount_of_each_card
 
 filename = "example.txt"
 
@@ -20,7 +20,7 @@ def test_part2():
     # When
     result = part2(lines)
     # Then
-    assert result == 0
+    assert result == 30
 
 
 def test_parse_cards():
@@ -30,13 +30,14 @@ def test_parse_cards():
     cards = parse_cards(lines)
     # Then
     assert len(cards) == 1
+    assert cards[0].id == 1
     assert cards[0].winning_numbers == {41, 48, 83, 86, 17}
     assert cards[0].numbers == {83, 86, 6, 31, 17, 9, 48, 53}
 
 
 def test_get_number_of_win():
     # Given
-    card = Card({41, 48, 83, 86, 17}, {83, 86, 6, 31, 17, 9, 48, 53})
+    card = Card(1, {41, 48, 83, 86, 17}, {83, 86, 6, 31, 17, 9, 48, 53})
     # When
     result = card.get_number_of_win()
     # Then
@@ -45,7 +46,7 @@ def test_get_number_of_win():
 
 def test_get_points_loosing_card():
     # Given
-    card = Card({87, 83, 26, 28, 32}, {88, 30, 70, 12, 93, 22, 82, 36})
+    card = Card(5, {87, 83, 26, 28, 32}, {88, 30, 70, 12, 93, 22, 82, 36})
     # When
     result = get_points(card)
     # Then
@@ -54,8 +55,22 @@ def test_get_points_loosing_card():
 
 def test_get_points_winning_card():
     # Given
-    card = Card({41, 48, 83, 86, 17}, {83, 86, 6, 31, 17, 9, 48, 53})
+    card = Card(1, {41, 48, 83, 86, 17}, {83, 86, 6, 31, 17, 9, 48, 53})
     # When
     result = get_points(card)
     # Then
     assert result == 8
+
+
+def test_get_amount_of_each_card():
+    # Given
+    # Card 1 makes us win one copy of card 2 and one of card 3
+    card1 = Card(1, {1, 2}, {1, 2})
+    # Card 2 makes us win one copy of card 3
+    card2 = Card(2, {1}, {1})
+    # Card 3 does not make us win anymore card
+    card3 = Card(3, {1}, {2})
+    # When
+    result = get_amount_of_each_card([card1, card2, card3])
+    # Then
+    assert result == {1: 1, 2: 2, 3: 4}
