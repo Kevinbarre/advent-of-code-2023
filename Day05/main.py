3e5edff1400af0754fdb1d6c0764a5f96dd66604
Kévin Barré <kevinbarre90@gmail.com>
2023-12-05 22:18:41 +0100

    Day 05 - Part 2

--- Day05/main.py
+++ Day05/main.py
@@ -7,7 +7,8 @@ def part1(lines):
 
 
 def part2(lines):
-    return 0
+    seed_ranges, maps = parse_input_2(lines)
+    return find_lowest_location_from_ranges(seed_ranges, maps)
 
 
 def parse_lines(lines):
@@ -19,6 +20,17 @@ def parse_lines(lines):
 def parse_seeds(raw_initial_seeds):
     _, seeds = raw_initial_seeds.split(":")
     return [int(seed) for seed in seeds.split()]
+
+
+def parse_seed_ranges(raw_initial_seeds):
+    _, raw_seeds = raw_initial_seeds.split(":")
+    raw_seeds = raw_seeds.split()
+    seed_ranges = []
+    while raw_seeds:
+        start = int(raw_seeds.pop(0))
+        range_length = int(raw_seeds.pop(0))
+        seed_ranges.append(Range(start, range_length))
+    return seed_ranges
 
 
 class MapElement:
@@ -33,6 +45,52 @@ class MapElement:
             return source_value - self.source_range_start + self.destination_range_start
         return False
 
+    def map_range(self, range_value):
+        # Case 1 : Range start is strictly before MapElement start
+        if range_value.start < self.source_range_start:
+            # Case 1.1 : Range end is strictly before MapElement start
+            if range_value.end < self.source_range_start:
+                # No mapping, as the range is outside the map
+                return False
+            # Case 1.2 : Range end is before MapElement end
+            if range_value.end <= self.source_range_end:
+                # Mapped range has length of difference between range_value.end and source_range_start + 1
+                mapped_range = Range(self.destination_range_start,
+                                     range_value.end - self.source_range_start + 1)
+                # Still need to map the remaining range smaller than the map
+                remaining_ranges = [Range(range_value.start, self.source_range_start - range_value.start)]
+                return mapped_range, remaining_ranges
+            # Case 1.3 : Range end is after MapElement end
+            else:
+                # Mapped range has length of self
+                mapped_range = Range(self.destination_range_start, self.source_range_end - self.source_range_start + 1)
+                # Still need to map the remaining range before and after the map
+                remaining_ranges = [Range(range_value.start, self.source_range_start - range_value.start),
+                                    Range(self.source_range_end + 1, range_value.end - self.source_range_end)]
+                return mapped_range, remaining_ranges
+        # Case 2 : Range start is before MapElement end
+        elif range_value.start <= self.source_range_end:
+            # Case 2.1 : Range end is before MapElement end
+            if range_value.end <= self.source_range_end:
+                # Mapped range with offset of range_value.start - source_range_start and has length of input range
+                mapped_range = Range(self.destination_range_start + range_value.start - self.source_range_start,
+                                     range_value.end - range_value.start + 1)
+                # The whole range has been mapped, no remaining range
+                remaining_ranges = []
+                return mapped_range, remaining_ranges
+            # Case 2.2 : Range end is after MapElement end
+            else:
+                # Mapped range with offset of range_value.start - source_range_start has length of difference between source_range_end and range_value.start
+                mapped_range = Range(self.destination_range_start + range_value.start - self.source_range_start,
+                                     self.source_range_end - range_value.start + 1)
+                # Still need to map the remaining range after the map
+                remaining_ranges = [Range(self.source_range_end + 1, range_value.end - self.source_range_end)]
+                return mapped_range, remaining_ranges
+        # Case 3 : Range start is after MapElement end
+        else:
+            # No mapping, as the range is outside the map
+            return False
+
 
 class CompleteMap:
     def __init__(self, map_elements):
@@ -46,6 +104,41 @@ class CompleteMap:
                 return destination
         # No mapping found, default to returning the source value
         return source_value
+
+    def map_ranges(self, range_values):
+        ranges_to_map = range_values
+        mapped_ranges = []
+        for map_element in self.map_elements:
+            future_remaining_ranges = []
+            for range_to_map in ranges_to_map:
+                map_result = map_element.map_range(range_to_map)
+                if not map_result:
+                    # No mapping performed, we'll have to handle this range with the next MapElement
+                    future_remaining_ranges.append(range_to_map)
+                else:
+                    mapped_range, remaining_ranges = map_result
+                    mapped_ranges.append(mapped_range)
+                    future_remaining_ranges += remaining_ranges
+            if not future_remaining_ranges:
+                # No more ranges to map, we're done with this mapping step
+                return mapped_ranges
+            else:
+                # Moving on with the next MapElement
+                ranges_to_map = future_remaining_ranges
+        # Remaining range to map are kept as they are
+        return mapped_ranges + ranges_to_map
+
+
+class Range:
+    def __init__(self, start, length):
+        self.start = start
+        self.end = start + length - 1
+
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+    def __repr__(self):
+        return "Range(start=%r, end=%r)" % (self.start, self.end)
 
 
 def parse_maps(raw_maps):
@@ -75,6 +168,13 @@ def parse_input(lines):
     return seeds, maps
 
 
+def parse_input_2(lines):
+    raw_initial_seeds, raw_maps = parse_lines(lines)
+    seed_ranges = parse_seed_ranges(raw_initial_seeds)
+    maps = parse_maps(raw_maps)
+    return seed_ranges, maps
+
+
 def get_seed_location(seed, maps):
     seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location = maps
     soil = seed_to_soil.map_source(seed)
@@ -88,8 +188,27 @@ def get_seed_location(seed, maps):
     return location
 
 
+def get_seed_range_location_ranges(seed_range, maps):
+    seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location = maps
+    soil_ranges = seed_to_soil.map_ranges([seed_range])
+    fertilizer_ranges = soil_to_fertilizer.map_ranges(soil_ranges)
+    water_ranges = fertilizer_to_water.map_ranges(fertilizer_ranges)
+    light_ranges = water_to_light.map_ranges(water_ranges)
+    temperature_ranges = light_to_temperature.map_ranges(light_ranges)
+    humidity_ranges = temperature_to_humidity.map_ranges(temperature_ranges)
+    location_ranges = humidity_to_location.map_ranges(humidity_ranges)
+    return location_ranges
+
+
 def find_lowest_location(seeds, maps):
     return min(get_seed_location(seed, maps) for seed in seeds)
+
+
+def find_lowest_location_from_ranges(seed_ranges, maps):
+    location_ranges = []
+    for seed_range in seed_ranges:
+        location_ranges += get_seed_range_location_ranges(seed_range, maps)
+    return min(location_range.start for location_range in location_ranges)
 
 
 if __name__ == '__main__':
--- Day05/test_main.py
+++ Day05/test_main.py
@@ -1,7 +1,8 @@
 import pytest
 
 from main import part1, part2, parse_lines, parse_seeds, parse_maps, get_seed_location, find_lowest_location, \
-    parse_input, MapElement, CompleteMap
+    parse_input, MapElement, CompleteMap, parse_seed_ranges, parse_input_2, Range, get_seed_range_location_ranges, \
+    find_lowest_location_from_ranges
 
 filename = "example.txt"
 
@@ -23,7 +24,7 @@ def test_part2():
     # When
     result = part2(lines)
     # Then
-    assert result == 0
+    assert result == 46
 
 
 def test_parse_lines():
@@ -223,3 +224,202 @@ def test_complete_map_map_source(test_input, expected):
     result = complete_map.map_source(test_input)
     # Then
     assert result == expected
+
+
+def test_parse_seeds_range():
+    # Given
+    raw_initial_seeds = "seeds: 79 14 55 13"
+    # When
+    result = parse_seed_ranges(raw_initial_seeds)
+    # Then
+    assert len(result) == 2
+    first_range = result[0]
+    assert first_range.start == 79
+    assert first_range.end == 92
+    second_range = result[1]
+    assert second_range.start == 55
+    assert second_range.end == 67
+
+
+def test_parse_input_range():
+    # Given
+    lines = ["seeds: 79 14 55 13", "", "seed-to-soil map:", "50 98 2", "52 50 3", "", "soil-to-fertilizer map:",
+             "0 15 1", "37 52 2", "39 0 3"]
+    # When
+    seed_ranges, maps = parse_input_2(lines)
+    # Then
+    assert len(seed_ranges) == 2
+    first_range = seed_ranges[0]
+    assert first_range.start == 79
+    assert first_range.end == 92
+    second_range = seed_ranges[1]
+    assert second_range.start == 55
+    assert second_range.end == 67
+    assert len(maps) == 2
+    # Seed to soil map
+    complete_map_1 = maps[0]
+    assert len(complete_map_1.map_elements) == 2
+    map_element_1 = complete_map_1.map_elements[0]
+    assert map_element_1.source_range_start == 98
+    assert map_element_1.source_range_end == 99
+    assert map_element_1.destination_range_start == 50
+    map_element_2 = complete_map_1.map_elements[1]
+    assert map_element_2.source_range_start == 50
+    assert map_element_2.source_range_end == 52
+    assert map_element_2.destination_range_start == 52
+    # Soil to fertilizer map
+    complete_map_2 = maps[1]
+    map_element_3 = complete_map_2.map_elements[0]
+    assert map_element_3.source_range_start == 15
+    assert map_element_3.source_range_end == 15
+    assert map_element_3.destination_range_start == 0
+    map_element_4 = complete_map_2.map_elements[1]
+    assert map_element_4.source_range_start == 52
+    assert map_element_4.source_range_end == 53
+    assert map_element_4.destination_range_start == 37
+    map_element_5 = complete_map_2.map_elements[2]
+    assert map_element_5.source_range_start == 0
+    assert map_element_5.source_range_end == 2
+    assert map_element_5.destination_range_start == 39
+
+
+def test_equal_range():
+    # Given
+    range_1 = Range(10, 1)
+    range_2 = Range(10, 1)
+    # When
+    result = range_1 == range_2
+    # Then
+    assert result is True
+
+
+# Test for case 1.1 and case 3
+@pytest.mark.parametrize("test_input",
+                         [Range(10, 10), Range(31, 10)])
+def test_map_element_map_range_seed_range_outside_map(test_input):
+    # Given
+    map_element = MapElement(20, 30, 50)
+    # When
+    result = map_element.map_range(test_input)
+    # Then
+    assert result is False
+
+
+# Test for other cases
+@pytest.mark.parametrize("test_input, expected_mapped_range, expected_remaining_ranges", [
+    # Case 1.2
+    (Range(10, 15), Range(50, 5), [Range(10, 10)]),
+    # Case 1.3
+    (Range(10, 31), Range(50, 11), [Range(10, 10), Range(31, 10)]),
+    # Case 2.1
+    (Range(20, 8), Range(50, 8), []),
+    (Range(22, 6), Range(52, 6), []),
+    # Case 2.2
+    (Range(20, 15), Range(50, 11), [Range(31, 4)]),
+    (Range(22, 13), Range(52, 9), [Range(31, 4)])
+])
+def test_map_element_map_range_seed_inside_map(test_input, expected_mapped_range, expected_remaining_ranges):
+    # Given
+    map_element = MapElement(20, 30, 50)
+    # When
+    mapped_range, remaining_ranges = map_element.map_range(test_input)
+    # Then
+    assert mapped_range == expected_mapped_range
+    assert remaining_ranges == expected_remaining_ranges
+
+
+def test_map_element_map_range_single_element():
+    # Given
+    map_element = MapElement(79, 79, 81)
+    # When
+    mapped_range, remaining_ranges = map_element.map_range(Range(79, 1))
+    # Then
+    assert mapped_range == Range(81, 1)
+    assert remaining_ranges == []
+
+
+def test_map_element_map_range_single_element_bigger_map():
+    # Given
+    map_element = MapElement(50, 97, 52)
+    # When
+    mapped_range, remaining_ranges = map_element.map_range(Range(82, 1))
+    # Then
+    assert mapped_range == Range(84, 1)
+    assert remaining_ranges == []
+
+
+def test_complete_map_map_ranges():
+    # Given
+    seed_range = Range(15, 41)
+    assert seed_range.start == 15
+    assert seed_range.end == 55
+    map_element1 = MapElement(20, 30, 120)
+    map_element2 = MapElement(40, 50, 80)
+    complete_map = CompleteMap([map_element1, map_element2])
+    # When
+    result = complete_map.map_ranges([seed_range])
+    # Then
+    assert len(result) == 5
+    assert Range(15, 5) in result
+    assert Range(120, 11) in result
+    assert Range(31, 9) in result
+    assert Range(80, 11) in result
+    assert Range(51, 5) in result
+
+
+def test_complete_map_map_ranges_single_element():
+    # Given
+    seed_range = Range(79, 1)
+    complete_map = CompleteMap([MapElement(79, 79, 81)])
+    # When
+    result = complete_map.map_ranges([seed_range])
+    # Then
+    assert result == [Range(81, 1)]
+
+
+def test_get_seed_range_location_range():
+    # Given
+    maps = [
+        # Seed to soil
+        CompleteMap([MapElement(79, 79, 81)]),
+        # Soil to fertilizer
+        CompleteMap([MapElement(81, 81, 81)]),
+        # Fertilizer to water
+        CompleteMap([MapElement(81, 81, 81)]),
+        # Water to light
+        CompleteMap([MapElement(81, 81, 74)]),
+        # Light to temperature
+        CompleteMap([MapElement(74, 74, 78)]),
+        # Temperature to humidity
+        CompleteMap([MapElement(78, 78, 78)]),
+        # Humidity to location
+        CompleteMap([MapElement(78, 78, 82)]),
+    ]
+    # When
+    result = get_seed_range_location_ranges(Range(79, 1), maps)
+    # Then
+    assert result == [Range(82, 1)]
+
+
+def test_find_lowest_location_from_ranges():
+    # Given
+    seed_ranges = [Range(79, 1), Range(14, 1)]
+    maps = [  # Seed to soil
+        CompleteMap([MapElement(79, 79, 81)]),
+        # Soil to fertilizer
+        CompleteMap([MapElement(14, 14, 53)]),
+        # Fertilizer to water
+        CompleteMap([MapElement(53, 53, 49)]),
+        # Water to light
+        CompleteMap([MapElement(81, 81, 74), MapElement(49, 49, 42)]),
+        # Light to temperature
+        CompleteMap([MapElement(74, 74, 78)]),
+        # Temperature to humidity
+        CompleteMap([MapElement(42, 42, 43)]),
+        # Humidity to location
+        CompleteMap([MapElement(78, 78, 82)]),
+    ]
+    # When
+    result = find_lowest_location_from_ranges(seed_ranges, maps)
+    # Then
+    assert result == 43
