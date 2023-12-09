ebc239535114ea65b66ed43948ccece2ae0938bd
Kévin Barré <kevinbarre90@gmail.com>
2023-12-09 17:44:12 +0100

    Day 09 - Part 1

---
+++ Day09/main.py
@@ -0,0 +1,37 @@
+def part1(lines):
+    sequences = parse_sequences(lines)
+    return sum(get_next_value(sequence) for sequence in sequences)
+
+
+def part2(lines):
+    return 0
+
+
+def parse_sequences(lines):
+    return [list(map(int, line.split())) for line in lines]
+
+
+def compute_differences(sequence):
+    return [next_one - previous for previous, next_one in zip(sequence, sequence[1:])]
+
+
+def get_all_differences(sequence):
+    all_differences = []
+    last_differences = sequence
+    while not all(element == 0 for element in last_differences):
+        last_differences = compute_differences(last_differences)
+        all_differences.append(last_differences)
+    return all_differences
+
+
+def get_next_value(sequence):
+    all_differences = get_all_differences(sequence)
+    return sequence[-1] + sum(differences[-1] for differences in all_differences)
+
+
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        f_lines = f.read().splitlines()
+
+    print("Part 1 : ", part1(f_lines))
+    print("Part 2 : ", part2(f_lines))
---
+++ Day09/test_main.py
@@ -0,0 +1,59 @@
+from main import part1, part2, parse_sequences, compute_differences, get_all_differences, get_next_value
+
+filename = "example.txt"
+
+
+def test_part1():
+    # Given
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    # When
+    result = part1(lines)
+    # Then
+    assert result == 114
+
+
+def test_part2():
+    # Given
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    # When
+    result = part2(lines)
+    # Then
+    assert result == 0
+
+
+def test_parse_sequences():
+    # Given
+    lines = ["0 3 6 9 12 15", "1 3 6 10 15 21", "10 13 16 21 30 45"]
+    # When
+    result = parse_sequences(lines)
+    # Then
+    assert result == [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+
+
+def test_compute_differences():
+    # Given
+    sequence = [0, 3, 6, 9, 12, 15]
+    # When
+    result = compute_differences(sequence)
+    # Then
+    assert result == [3, 3, 3, 3, 3]
+
+
+def test_get_all_differences():
+    # Given
+    sequence = [10, 13, 16, 21, 30, 45]
+    # When
+    result = get_all_differences(sequence)
+    # Then
+    assert result == [[3, 3, 5, 9, 15], [0, 2, 4, 6], [2, 2, 2], [0, 0]]
+
+
+def test_get_next_value():
+    # Given
+    sequence = [10, 13, 16, 21, 30, 45]
+    # When
+    result = get_next_value(sequence)
+    # Then
+    assert result == 68
