ca59754d8c39f9345469b9e0155dd229e389aa94
Kévin Barré <kevinbarre90@gmail.com>
2023-12-14 19:28:54 +0100

    Day 14 - Part 2

--- Day14/main.py
+++ Day14/main.py
@@ -4,19 +4,20 @@ def part1(lines):
 
 
 def part2(lines):
-    return 0
+    grid = cycle_multiple_times(lines, 1000000000)
+    return get_load(grid)
 
 
 def transpose_grid(grid):
     return [''.join(column) for column in zip(*grid)]
 
 
-def sort_row(row):
-    return ''.join(sorted(row, reverse=True))
+def sort_row(row, reverse):
+    return ''.join(sorted(row, reverse=reverse))
 
 
-def sort_full_row(full_row):
-    return '#'.join(sort_row(row) for row in full_row.split('#'))
+def sort_full_row(full_row, reverse=True):
+    return '#'.join(sort_row(row, reverse) for row in full_row.split('#'))
 
 
 def tilt_north(grid):
@@ -25,12 +26,48 @@ def tilt_north(grid):
     return transpose_grid(transposed)
 
 
+def tilt_west(grid):
+    return [sort_full_row(full_row) for full_row in grid]
+
+
+def tilt_south(grid):
+    transposed = transpose_grid(grid)
+    transposed = [sort_full_row(full_row, False) for full_row in transposed]
+    return transpose_grid(transposed)
+
+
+def tilt_east(grid):
+    return [sort_full_row(full_row, False) for full_row in grid]
+
+
 def get_load(grid):
     total = 0
     depth = len(grid)
     for j, row in enumerate(grid):
         total += sum(depth - j for rock in row if rock == "O")
     return total
+
+
+def cycle_once(grid):
+    return tuple(tilt_east(tilt_south(tilt_west(tilt_north(grid)))))
+
+
+def cycle_multiple_times(grid, nb_cycle):
+    new_grid = tuple(grid)
+    encountered_grids = {}
+    for i in range(nb_cycle):
+        if new_grid in encountered_grids:
+            # Cycle found, no need to compute anymore cycles
+            cycle_start = encountered_grids[new_grid]
+            cycle_length = i - cycle_start
+            final_index = ((nb_cycle - cycle_start) % cycle_length) + cycle_start
+            target_grid = list(encountered_grids.keys())[
+                list(encountered_grids.values()).index(final_index)]
+            return target_grid
+        else:
+            encountered_grids[new_grid] = i
+        new_grid = cycle_once(new_grid)
+    return new_grid
 
 
 if __name__ == '__main__':
--- Day14/test_main.py
+++ Day14/test_main.py
@@ -1,4 +1,7 @@
-from main import part1, part2, transpose_grid, sort_row, sort_full_row, tilt_north, get_load
+import pytest
+
+from main import part1, part2, transpose_grid, sort_row, sort_full_row, tilt_north, get_load, tilt_west, tilt_south, \
+    tilt_east, cycle_once, cycle_multiple_times
 
 filename = "example.txt"
 
@@ -20,7 +23,7 @@ def test_part2():
     # When
     result = part2(lines)
     # Then
-    assert result == 0
+    assert result == 64
 
 
 def test_transpose_grid():
@@ -39,9 +42,18 @@ def test_sort_row():
     # Given
     row = "O...O..O."
     # When
-    result = sort_row(row)
+    result = sort_row(row, True)
     # Then
     assert result == "OOO......"
+
+
+def test_sort_row_not_reverse():
+    # Given
+    row = "O...O..O."
+    # When
+    result = sort_row(row, False)
+    # Then
+    assert result == "......OOO"
 
 
 def test_sort_full_row():
@@ -96,3 +108,159 @@ def test_get_load():
     result = get_load(grid)
     # Then
     assert result == 136
+
+
+def test_tilt_west():
+    # Given
+    grid = ["OOOO.#.O..",
+            "OO..#....#",
+            "OO..O##..O",
+            "O..#.OO...",
+            "........#.",
+            "..#....#.#",
+            "..O..#.O.O",
+            "..O.......",
+            "#....###..",
+            "#....#...."]
+    # When
+    result = tilt_west(grid)
+    # Then
+    assert result == ["OOOO.#O...",
+                      "OO..#....#",
+                      "OOO..##O..",
+                      "O..#OO....",
+                      "........#.",
+                      "..#....#.#",
+                      "O....#OO..",
+                      "O.........",
+                      "#....###..",
+                      "#....#...."]
+
+
+def test_tilt_south():
+    # Given
+    grid = ["OOOO.#O...",
+            "OO..#....#",
+            "OOO..##O..",
+            "O..#OO....",
+            "........#.",
+            "..#....#.#",
+            "O....#OO..",
+            "O.........",
+            "#....###..",
+            "#....#...."]
+    # When
+    result = tilt_south(grid)
+    # Then
+    assert result == [".....#....",
+                      "....#.O..#",
+                      "O..O.##...",
+                      "O.O#......",
+                      "O.O....O#.",
+                      "O.#..O.#.#",
+                      "O....#....",
+                      "OO....OO..",
+                      "#O...###..",
+                      "#O..O#...."]
+
+
+def test_tilt_east():
+    # Given
+    grid = [".....#....",
+            "....#.O..#",
+            "O..O.##...",
+            "O.O#......",
+            "O.O....O#.",
+            "O.#..O.#.#",
+            "O....#....",
+            "OO....OO..",
+            "#O...###..",
+            "#O..O#...."]
+    # When
+    result = tilt_east(grid)
+    # Then
+    assert result == [".....#....",
+                      "....#...O#",
+                      "...OO##...",
+                      ".OO#......",
+                      ".....OOO#.",
+                      ".O#...O#.#",
+                      "....O#....",
+                      "......OOOO",
+                      "#...O###..",
+                      "#..OO#...."]
+
+
+def test_cycle_once():
+    # Given
+    grid = ("O....#....",
+            "O.OO#....#",
+            ".....##...",
+            "OO.#O....O",
+            ".O.....O#.",
+            "O.#..O.#.#",
+            "..O..#O..O",
+            ".......O..",
+            "#....###..",
+            "#OO..#....")
+    # When
+    result = cycle_once(grid)
+    # Then
+    assert result == (".....#....",
+                      "....#...O#",
+                      "...OO##...",
+                      ".OO#......",
+                      ".....OOO#.",
+                      ".O#...O#.#",
+                      "....O#....",
+                      "......OOOO",
+                      "#...O###..",
+                      "#..OO#....")
+
+
+@pytest.mark.parametrize("test_input, expected", [(1, (".....#....",
+                                                       "....#...O#",
+                                                       "...OO##...",
+                                                       ".OO#......",
+                                                       ".....OOO#.",
+                                                       ".O#...O#.#",
+                                                       "....O#....",
+                                                       "......OOOO",
+                                                       "#...O###..",
+                                                       "#..OO#....")),
+                                                  (2, (".....#....",
+                                                       "....#...O#",
+                                                       ".....##...",
+                                                       "..O#......",
+                                                       ".....OOO#.",
+                                                       ".O#...O#.#",
+                                                       "....O#...O",
+                                                       ".......OOO",
+                                                       "#..OO###..",
+                                                       "#.OOO#...O")),
+                                                  (3, (".....#....",
+                                                       "....#...O#",
+                                                       ".....##...",
+                                                       "..O#......",
+                                                       ".....OOO#.",
+                                                       ".O#...O#.#",
+                                                       "....O#...O",
+                                                       ".......OOO",
+                                                       "#...O###.O",
+                                                       "#.OOO#...O"))])
+def test_cycle_multiple_times(test_input, expected):
+    # Given
+    grid = ("O....#....",
+            "O.OO#....#",
+            ".....##...",
+            "OO.#O....O",
+            ".O.....O#.",
+            "O.#..O.#.#",
+            "..O..#O..O",
+            ".......O..",
+            "#....###..",
+            "#OO..#....")
+    # When
+    result = cycle_multiple_times(grid, test_input)
+    # Then
+    assert result == expected
