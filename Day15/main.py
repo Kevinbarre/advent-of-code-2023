95e36d99c094f03a963caa886ac9904aa0ec37f8
Kévin Barré <kevinbarre90@gmail.com>
2023-12-15 22:41:00 +0100

    Day 15 - Part 2

--- Day15/main.py
+++ Day15/main.py
@@ -1,10 +1,15 @@
+from collections import OrderedDict
+
+
 def part1(lines):
     steps = parse_steps(lines[0])
     return sum(hash_algorithm(step) for step in steps)
 
 
 def part2(lines):
-    return 0
+    steps = parse_steps(lines[0])
+    boxes = initialize_boxes(steps)
+    return sum(get_focusing_power(boxes, box_number) for box_number in boxes)
 
 
 def parse_steps(steps):
@@ -18,6 +23,43 @@ def hash_algorithm(string):
         current_value *= 17
         current_value %= 256
     return current_value
+
+
+def add_lens(boxes, label, focal):
+    box_number = hash_algorithm(label)
+    if box_number not in boxes:
+        boxes[box_number] = OrderedDict()
+    boxes[box_number][label] = focal
+
+
+def remove_lens(boxes, label):
+    box_number = hash_algorithm(label)
+    try:
+        del boxes[box_number][label]
+    except KeyError:
+        # Ignore deleting a non existing label / box
+        pass
+
+
+def initialize_boxes(steps):
+    boxes = {}
+    for step in steps:
+        if "=" in step:
+            # Add lens
+            label, focal = step.split("=")
+            add_lens(boxes, label, int(focal))
+        else:
+            # Remove lens
+            remove_lens(boxes, step[:-1])
+    return boxes
+
+
+def get_focusing_power(boxes, box_number):
+    box = boxes[box_number]
+    focusing_power = 0
+    for i, label in enumerate(box):
+        focusing_power += (box_number + 1) * (i + 1) * box[label]
+    return focusing_power
 
 
 if __name__ == '__main__':
--- Day15/test_main.py
+++ Day15/test_main.py
@@ -1,4 +1,8 @@
-from main import part1, part2, parse_steps, hash_algorithm
+from collections import OrderedDict
+
+import pytest
+
+from main import part1, part2, parse_steps, hash_algorithm, add_lens, remove_lens, initialize_boxes, get_focusing_power
 
 filename = "example.txt"
 
@@ -20,7 +24,7 @@ def test_part2():
     # When
     result = part2(lines)
     # Then
-    assert result == 0
+    assert result == 145
 
 
 def test_parse_steps():
@@ -39,3 +43,62 @@ def test_hash_algorithm():
     result = hash_algorithm(string)
     # Then
     assert result == 52
+
+
+def test_add_lens():
+    # Given
+    boxes = {}
+    # When
+    add_lens(boxes, "cm", 1)
+    # Then
+    assert boxes == {0: {"cm": 1}}
+
+
+def test_add_lens_replace_old():
+    # Given
+    boxes = {0: OrderedDict({"cm": 1})}
+    # When
+    add_lens(boxes, "cm", 9)
+    # Then
+    assert boxes == {0: {"cm": 9}}
+
+
+def test_remove_lens():
+    # Given
+    boxes = {0: OrderedDict({"cm": 1})}
+    # When
+    remove_lens(boxes, "cm")
+    # Then
+    assert boxes == {0: {}}
+
+
+def test_remove_lens_from_non_existing_box():
+    # Given
+    boxes = {}
+    # When
+    remove_lens(boxes, "cm")
+    # Then
+    assert boxes == {}
+
+
+def test_initialize_boxes():
+    # Given
+    steps = ["rn=1", "cm-", "qp=3", "cm=2", "qp-", "pc=4", "ot=9", "ab=5", "pc-", "pc=6", "ot=7"]
+    # When
+    result = initialize_boxes(steps)
+    # Then
+    assert result == {0: OrderedDict({"rn": 1, "cm": 2}),
+                      1: OrderedDict(),
+                      3: OrderedDict({"ot": 7, "ab": 5, "pc": 6})}
+
+
+@pytest.mark.parametrize("test_input, expected", [(0, 5), (1, 0), (3, 140)])
+def test_get_focusing_power(test_input, expected):
+    # Given
+    boxes = {0: OrderedDict({"rn": 1, "cm": 2}),
+             1: OrderedDict(),
+             3: OrderedDict({"ot": 7, "ab": 5, "pc": 6})}
+    # When
+    result = get_focusing_power(boxes, test_input)
+    # Then
+    assert result == expected
